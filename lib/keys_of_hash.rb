88806d6c5dc9fe03d22d503e2b1ed98d9289f127
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 19:51:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
     hash_values.each do |value|
       arguments.each do |argument|
         if value == argument
-          puts value
+          puts Hash.find_index(value)
         end
       end
     end
