d432647cfaaeb4b7e55d21d1ec6788c6fc5f7544
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 19:37:14 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
     hash_values = self.values
     hash_values.select do |values|
       values == arguments
-      puts values.to_s +":"+ arguments.to_s
+      puts values +":"+ arguments
     end
   end
 end
