e6f88cdda68626445942eaca32dbef954e1688d2
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-08-24 19:32:10 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -9,4 +9,4 @@ end
 
 animals =  {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
 
-puts :animals.keys_of('Panama')
+puts animals.keys_of('Panama')
